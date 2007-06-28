31dda3114bd4eb5043fc581786df2c01f1faaa25
martin <martin@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-06-28 08:11:09 +0000

    - removed old examples

--- enthought/envisage/examples/apps/Acme Lab/plugins/acme.workbench/acme/workbench/view/api.py
+++
@@ -1,5 +0,0 @@
-from black_view import BlackView
-from blue_view import BlueView
-from green_view import GreenView
-from red_view import RedView
-from yellow_view import YellowView
--- enthought/envisage/examples/apps/MOTD/plugins/acme.motd/acme/motd/api.py
+++
@@ -1,5 +0,0 @@
-from i_message import IMessage
-
-from extensible_motd import ExtensibleMOTD
-from message import Message
-from motd import MOTD
