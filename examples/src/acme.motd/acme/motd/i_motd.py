7b506fb0423f7846de06b71feae10b194b22d78c
martin <martin@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-07-01 14:49:58 +0000

    - added missing module

---
+++ examples/src/acme.motd/acme/motd/i_motd.py
@@ -0,0 +1,18 @@
+""" The 'Message of the Day' interface. """
+
+
+# Enthought library imports.
+from enthought.traits.api import Interface
+
+
+class IMOTD(Interface):
+    """ The 'Message of the Day' interface. """
+
+    def motd(self):
+        """ Return the message of the day.
+
+        Returns an object that implements the 'IMessage' interface.
+
+        """
+    
+#### EOF ######################################################################
