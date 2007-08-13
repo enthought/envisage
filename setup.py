bd2216e568878c61ed724764e6c77f88ce9c160e
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-08-13 00:07:13 +0000

    * Egg-building infrastructure improvements (adding README.txt, LICENSE.txt, setup.cfg, etc.; re-ordering setup keywords to be alphabetical; updating dependencies to use the etsdeps function to allow dev builds to match; etc.)

--- setup.py
+++ setup.py
@@ -1,24 +1,54 @@
 from setuptools import setup, find_packages
 
 
-setup(
-    name                 = "enthought.envisage",
-    version              = "3.0a1",
-    description          = "Envisage - An Extensible Application Framework",
-    url                  = "http://code.enthought.com/enstaller",
-    license              = "BSD",
-    zip_safe             = False,
-    packages             = find_packages(),
-    ext_modules          = [],
-    include_package_data = True,
+# Function to convert simple ETS project names and versions to a requirements
+# spec that works for both development builds and stable builds.  Allows
+# a caller to specify a max version, which is intended to work along with
+# Enthought's standard versioning scheme -- see the following write up:
+#    https://svn.enthought.com/enthought/wiki/EnthoughtVersionNumbers
+def etsdep(p, min, max=None, literal=False):
+    require = '%s >=%s.dev' % (p, min)
+    if max is not None:
+        if literal is False:
+            require = '%s, <%s.a' % (require, max)
+        else:
+            require = '%s, <%s' % (require, max)
+    return require
 
+
+# Declare our ETS project dependencies.
+TRAITS = etsdep('enthought.traits', '3.0.0b1')
+UTIL = etsdep('enthought.util', '3.0.0a1')
+
+
+setup(
+    author = "Martin Chilvers",
+    author_email = "info@enthought.com",
+    dependency_links = [
+        'http://code.enthought.com/enstaller/eggs/source',
+        'http://code.enthought.com/enstaller/eggs/source/unstable',
+        ],
+    description = "Envisage - An Extensible Application Framework",
+    extras_require = {
+        # All non-ets dependencies should be in this extra to ensure users can
+        # decide whether to require them or not.
+        'nonets': [
+            ],
+        },
+    ext_modules = [],
+    include_package_data = True,
+    install_requires = [
+        TRAITS,
+        UTIL,
+        ],
+    license = "BSD",
+    name = "enthought.envisage",
     namespace_packages   = [
         "enthought",
         "enthought.envisage"
-    ],
-
-    install_requires     = [
-        "enthought.traits>=3.0.0b1",
-        "enthought.util"
-    ]
-)
+        ],
+    packages = find_packages(),
+    url = "http://code.enthought.com/envisage",
+    version = "3.0.0a1",
+    zip_safe = False,
+    )
