0e6a28963d0c0b76edd0c17395d60129e60f17f5
martin <martin@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-08-19 16:29:48 +0000

    - added file resource to api.py

--- enthought/envisage/resource/api.py
+++ enthought/envisage/resource/api.py
@@ -1,6 +1,7 @@
 from i_resource_protocol import IResourceProtocol
 from i_resource_manager import IResourceManager
 
+from file_resource_protocol import FileResourceProtocol
 from http_resource_protocol import HTTPResourceProtocol
 from package_resource_protocol import PackageResourceProtocol
 from resource_manager import ResourceManager
