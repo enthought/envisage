db1f2587dda53728d62b2f45d85f9317fa7e9353
martin <martin@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-09-05 14:04:45 +0000

    - fixed reST errors in docstrings...

--- enthought/envisage/i_application.py
+++ enthought/envisage/i_application.py
@@ -67,21 +67,21 @@ class IApplication(Interface):
 
         1) 'foo.bar.baz'
 
-        Which is turned into the equivalent of an import statement that
-        looks like::
+           Which is turned into the equivalent of an import statement that
+           looks like::
 
-            from foo.bar import baz
+             from foo.bar import baz
 
-        With the value of 'baz' being returned.
+           With the value of 'baz' being returned.
 
         2) 'foo.bar:baz' (i.e. a ':' separating the module from the symbol)
 
-        Which is turned into the equivalent of::
+           Which is turned into the equivalent of::
 
-            from foo import bar
-            eval('baz', bar.__dict__)
+             from foo import bar
+             eval('baz', bar.__dict__)
 
-        With the result of the 'eval' being returned.
+           With the result of the 'eval' being returned.
 
         The second form is recommended as it allows for nested symbols to be
         retreived, e.g. the symbol path 'foo.bar:baz.bling' becomes::
