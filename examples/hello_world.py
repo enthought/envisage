6b20c85664ba689bc993e967a85f5ac50967f28c
martin <martin@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-10-08 17:04:53 +0000

    - use a trait initializer to show how to dynamically create contributions to an extension point

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -9,41 +9,57 @@ from enthought.traits.api import List, Str
 class HelloWorld(Plugin):
     """ A plugin that offers an extension point. """
 
-    # This tells us that this plugin offers the 'greetings' extension point,
-    # and that each plugin that wants to contribute to it must provide a list
+    # This tells us that the plugin offers the 'greetings' extension point,
+    # and that plugins that want to contribute to it must each provide a list
     # of strings (Str).
     greetings = ExtensionPoint(
         List(Str), id='greetings', desc='Greetings for "Hello World"'
     )
 
     # Plugin's have two important lifecyle methods, 'start' and 'stop'.
-    # These methods can be called manually, but usually they are called when
-    # the application is started and stopped respectively.
+    # These methods can be called manually, but usually they are called
+    # automatically by Envisage when the application is started and stopped
+    # respectively.
     def start(self):
         """ Start the plugin. """
 
         # Standard library imports.
         import random
 
+        print self.greetings
         print random.choice(self.greetings), 'World!'
+
         
         return
 
 
 class Greetings(Plugin):
-    """ A plugin that contributes to the extension point. """
+    """ A plugin that contributes to the 'greetings' extension point. """
 
-    # This tells us that the plugin contributes to the 'greetings' extension
-    # point using this trait.
+    # This tells us that the plugin contributes the value of this trait to the
+    # 'greetings' extension point.
     greetings = List(["Hello", "G'day"], extension_point='greetings')
 
 
 class MoreGreetings(Plugin):
-    """ Another plugin that contributes to the extension point. """
+    """ Another plugin that contributes to the 'greetings' extension point. """
 
-    # This tells us that the plugin contributes to the 'greetings' extension
-    # point using this trait.
-    greetings = List(['Bonjour', 'Ola'], extension_point='greetings')
+    # This tells us that the plugin contributes the value of this trait to the
+    # 'greetings' extension point.
+    greetings = List(extension_point='greetings')
+
+    # This shows how you can use a standard trait initializer to populate the
+    # list dynamically.
+    def _greetings_default(self):
+        """ Trait initializer. """
+
+        extensions = [
+            'The %s application says %s' % (self.application.id, greeting)
+
+            for greeting in ['Bonjour', 'Ola']
+        ]
+                             
+        return extensions
 
 
 # Application entry point.
