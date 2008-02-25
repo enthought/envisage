0b4777f113fe4cc8b878d615b4d88d681fac437f
martin <martin@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-02-25 16:40:36 +0000

    - added 'WorkbenchActionSet' removed 'DefaultActionSet' (it breaks lazy loading!)

--- enthought/envisage/ui/workbench/api.py
+++ enthought/envisage/ui/workbench/api.py
@@ -1,8 +1,8 @@
 """ Envisage package Copyright 2003, 2004, 2005 Enthought, Inc. """
 
 
-from default_action_set import DefaultActionSet
 from workbench import Workbench
+from workbench_action_set import WorkbenchActionSet
 from workbench_application import WorkbenchApplication
 from workbench_preferences_manager import WorkbenchPreferencesManager
 from workbench_window import WorkbenchWindow
