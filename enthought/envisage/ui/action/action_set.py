e7ec3ae22d0b182e16b4ebb6e968183cf549cd4a
martin <martin@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-02-25 18:18:54 +0000

    - experimental lifecycle method ('initialize') on ActionSet

--- enthought/envisage/ui/action/action_set.py
+++ enthought/envisage/ui/action/action_set.py
@@ -3,7 +3,6 @@
 
 # Enthought library imports.
 from enthought.traits.api import Bool, Dict, HasTraits, List, Str, implements
-from enthought.traits.api import on_trait_change
 
 # Local imports.
 from action import Action
@@ -40,11 +39,12 @@ class ActionSet(HasTraits):
     # The tool bars in this set.
     tool_bars = List(ToolBar)
 
-    # Are the actions and menus in this set enabled (if they are diabled they
-    # will be greyed out).
+    # Are the actions and menus in this set enabled (if they are disabled they
+    # will be greyed out). Tool bars are generally not greyed out themselves,
+    # but the actions within them are.
     enabled = Bool(True)
 
-    # Are the actions, menus and toolbars in this set visible?
+    # Are the actions, menus and tool bars in this set visible?
     visible = Bool(True)
     
     # A mapping from human-readable names to globally unique IDs.
@@ -82,58 +82,13 @@ class ActionSet(HasTraits):
     # 'ActionSet' interface.
     ###########################################################################
 
-##     @on_trait_change('enabled,visible')
-##     def _state_changed(self, obj, trait_name, old, new):
-##         """ Static trait change handler. """
-
-##         self._update_tool_bars(obj, trait_name, new)
-##         self._update_actions(obj, trait_name, new)
-
-##         return
-
     def initialize(self, window):
         """ Called by the framework when the action set is added to a window.
 
         """
 
-
-
-        return
-    
-    ###########################################################################
-    # Private interface.
-    ###########################################################################
-
-    def _update_tool_bars(self, window, trait_name, value):
-        """ Update the state of the tool bars in the action set. """
-
-        for tool_bar_manager in window.tool_bar_managers:
-            if tool_bar_manager._action_set_ is action_set:
-                setattr(tool_bar_manager, trait_name, value)
-
-        return
-
-    def _update_actions(self, window, trait_name, value):
-        """ Update the state of the tool bars in the action set. """
-
-        def visitor(item):
-            """ Called when we visit each item in an action manager. """
-
-            # fixme: The 'additions' group gets created by default and hence
-            # has no '_action_set_' attribute. This smells because of the
-            # fact that we 'tag' the '_action_set_' attribute onto all items to
-            # be able to find them later. This link should be maintained
-            # externally (maybe in the action set itself?).
-            if hasattr(item, '_action_set_'):
-                if item._action_set_ is action_set:
-                    setattr(item, trait_name, value)
-
-        window.menu_bar_manager.walk(visitor)
-
-        for tool_bar_manager in window.tool_bar_managers:
-            tool_bar_manager.walk(visitor)
-
-        return
+        # By default, do nothing!
+        pass
 
     ###########################################################################
     # Testing interface.
@@ -142,6 +97,7 @@ class ActionSet(HasTraits):
     def test(self):
         """ Testing! """
 
+        # By default do nothing!
         pass
     
 #### EOF ######################################################################
--- enthought/envisage/ui/workbench/workbench_window.py
+++ enthought/envisage/ui/workbench/workbench_window.py
@@ -37,6 +37,9 @@ class WorkbenchWindow(pyface.WorkbenchWindow):
     #### Private interface ####################################################
 
     # The workbench menu and tool bar builder.
+    #
+    # The builder is used to create the window's tool bar and menu bar by
+    # combining all of the contributed action sets.
     _action_manager_builder = Instance(WorkbenchActionManagerBuilder)
     
     # Contributed action sets.
@@ -57,14 +60,14 @@ class WorkbenchWindow(pyface.WorkbenchWindow):
     
     def _menu_bar_manager_default(self):
         """ Trait initializer. """
-
+        
         return self._action_manager_builder.create_menu_bar_manager('MenuBar')
-
+    
     def _tool_bar_managers_default(self):
         """ Trait initializer. """
 
         return self._action_manager_builder.create_tool_bar_managers('ToolBar')
-
+    
     ###########################################################################
     # 'pyface.WorkbenchWindow' interface.
     ###########################################################################
@@ -123,14 +126,6 @@ class WorkbenchWindow(pyface.WorkbenchWindow):
             else:
                 action_set = factory_or_action_set
 
-            action_set.on_trait_change(
-                self._on_action_set_state_changed, 'enabled'
-            )
-
-            action_set.on_trait_change(
-                self._on_action_set_state_changed, 'visible'
-            )
-
             action_sets.append(action_set)
 
         return action_sets
@@ -138,8 +133,6 @@ class WorkbenchWindow(pyface.WorkbenchWindow):
     ###########################################################################
     # Private interface.
     ###########################################################################
-
-    #### Trait initializers ###################################################
 
     def __action_manager_builder_default(self):
         """ Trait initializer. """
@@ -152,48 +145,5 @@ class WorkbenchWindow(pyface.WorkbenchWindow):
             action_set.initialize(self)
             
         return action_manager_builder
-
-    #### Trait change handlers ################################################
-
-    def _on_action_set_state_changed(self, obj, trait_name, old, new):
-        """ Dynamic trait change handler. """
-
-        self._update_tool_bars(obj, trait_name, new)
-        self._update_actions(obj, trait_name, new)
-
-        return
-
-    #### Methods ##############################################################
-
-    def _update_tool_bars(self, action_set, trait_name, value):
-        """ Update the state of the tool bars in an action set. """
-
-        for tool_bar_manager in self.tool_bar_managers:
-            if tool_bar_manager._action_set_ is action_set:
-                setattr(tool_bar_manager, trait_name, value)
-
-        return
-
-    def _update_actions(self, action_set, trait_name, value):
-        """ Update the state of the tool bars in an action set. """
-
-        def visitor(item):
-            """ Called when we visit each item in an action manager. """
-
-            # fixme: The 'additions' group gets created by default and hence
-            # has no '_action_set_' attribute. This smells because of the
-            # fact that we 'tag' the '_action_set_' attribute onto all items to
-            # be able to find them later. This link should be maintained
-            # externally (maybe in the action set itself?).
-            if hasattr(item, '_action_set_'):
-                if item._action_set_ is action_set:
-                    setattr(item, trait_name, value)
-
-        self.menu_bar_manager.walk(visitor)
-
-        for tool_bar_manager in self.tool_bar_managers:
-            tool_bar_manager.walk(visitor)
-
-        return
     
 #### EOF ######################################################################
