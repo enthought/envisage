ae3945b8041ad3490521fd4e792a2618e7e73861
martin <martin@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-03-19 12:36:42 +0000

    - cleanup

--- examples/MOTD/run.py
+++ examples/MOTD/run.py
@@ -4,15 +4,9 @@
 # Standard library imports.
 import logging
 
-
-
 # Enthought library imports.
 from enthought.envisage.api import Application
 
-# Do whatever you want to do with log messages! Here we create a log file.
-logger = logging.getLogger()
-logger.addHandler(logging.StreamHandler(file('acme_motd.log', 'w')))
-logger.setLevel(logging.DEBUG)
 
 # Enthought plugins.
 from enthought.envisage.core_plugin import CorePlugin
@@ -22,14 +16,16 @@ from acme.motd.motd_plugin import MOTDPlugin
 from acme.motd.software_quotes.software_quotes_plugin import SoftwareQuotesPlugin
 
 
+# Do whatever you want to do with log messages! Here we create a log file.
+logger = logging.getLogger()
+logger.addHandler(logging.StreamHandler(file('acme_motd.log', 'w')))
+logger.setLevel(logging.DEBUG)
 
 
 def main():
     """ Run the application. """
 
-
-    # Create an application that uses the egg plugin manager to find its
-    # plugins.
+    # Create an application containing the approriate plugins.
     application = Application(
         id      = 'acme.motd',
         plugins = [CorePlugin(), MOTDPlugin(), SoftwareQuotesPlugin()]
