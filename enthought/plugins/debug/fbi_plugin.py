3804a6a55ace95c6a5cfc89192d295a6252f231f
rkern <rkern@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-05 02:27:13 +0000

    Fix the debug plugin for Envisage 3.

--- enthought/plugins/debug/fbi_plugin.py
+++ enthought/plugins/debug/fbi_plugin.py
@@ -18,11 +18,9 @@ import sys
 
 from enthought.traits.api import push_exception_handler
 
-from enthought.envisage \
-    import Plugin
-    
-from enthought.debug.fbi \
-    import enable_fbi, fbi
+from enthought.envisage.api import Plugin
+
+from enthought.debug.fbi import enable_fbi, fbi
 
 #-------------------------------------------------------------------------------
 #  'FBIPlugin' class:  
