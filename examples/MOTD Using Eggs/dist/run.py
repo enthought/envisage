6f571547f622e4b10467223c2fe22d061e9c146b
dmartin <dmartin@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-11 19:59:11 +0000

    Added the MOTDPluginManager class which extends the EggPluginManager class. This class will only load plugins specific to the example and does not attempt to execute a .load() on any plugins it doesn't want, therefore avoiding the missing attribute error that was being caused by the mayavi_ui_plugin being loaded. The plugin_manager trait of the application now recieves a MOTDPluginManager instance instead of an EggPluginManager instance.

--- examples/MOTD Using Eggs/dist/run.py
+++ examples/MOTD Using Eggs/dist/run.py
@@ -9,36 +9,62 @@ Usually, all you have to do in here is:-
 
 3) Edit the 'run' function to do whatever you need to do to start your
    application.
-   
-"""
 
+"""
+# FIXME: This fix will work for now, but I believe there is a better way to do
+# this in which the loader would be smart enough to know what plugins it should
+# load without having to explicitly define them in 'include'. Also, the custom
+# __plugin_default method now looks at the entry point name instead of the
+# plugin.id, which is less specific therefore less preferable. This was done,
+# however, because the ep.name can be accessed before the ep.load() call, which
+# was causing problems when loading plugins not designed for this app. (dmartin)
 
 # Standard library imports.
 import logging
 
+# Enthought linrary imports
+from enthought.envisage.api import Application, EggPluginManager
+from enthought.envisage.egg_utils import get_entry_points_in_egg_order
 
 # Create a log file.
 logger = logging.getLogger()
 logger.addHandler(logging.StreamHandler(file('acme_motd.log', 'w')))
 logger.setLevel(logging.DEBUG)
 
-
 # A list of the directories that contain the application's eggs (any directory
 # not specified as an absolute path is treated as being relative to the current
 # working directory).
 EGG_PATH = ['eggs']
 
+class MOTDPluginManager(EggPluginManager):
+    """ A plugin manager that will get only the MOTD eggs """
+
+    # Only include the plugins necessary for the example
+    include = ['core', 'motd', 'software_quotes']
+
+    # Use a custom plugin loader that will NOT attempt to execute a .load on
+    # every plugin, therefore avoiding any errors that unwanted plugins may
+    # cause. Only execute a .load on entry points that are given in 'include'
+    def __plugins_default(self):
+        """ Trait initializer. """
+
+        plugins = []
+        for ep in get_entry_points_in_egg_order(self.working_set,self.PLUGINS):
+            if ep.name in self.include:
+                klass  = ep.load()
+                plugin = klass(application=self.application)
+                plugins.append(plugin)
+
+        logger.debug('motd plugin manager found plugins <%s>', plugins)
+
+        return plugins
 
 def run():
     """ The function that starts your application. """
 
-    # We do the imports here because the modules may well be in eggs that get
-    # added to the path in 'main'.
-    from enthought.envisage.api import Application, EggPluginManager
-
     # Create an application that uses the egg plugin manager to find its
     # plugins.
-    application = Application(id='acme.motd',plugin_manager=EggPluginManager())
+    application = Application(id='acme.motd',plugin_manager=MOTDPluginManager())
 
     # Run it!
     return application.run()
@@ -49,20 +75,17 @@ def run():
 ###############################################################################
 
 # Standard library imports.
-import logging
 from pkg_resources import Environment, working_set
-
 
 # Logging.
 logger = logging.getLogger(__name__)
 
-
 def main():
     """ Run the application. """
-    
+
     # Find all additional eggs.
     environment = Environment(EGG_PATH)
-    
+
     distributions, errors = working_set.find_plugins(environment)
     if len(errors) > 0:
         raise SystemError('cannot add eggs %s' % errors)
@@ -74,7 +97,6 @@ def main():
 
     # Create and run the application.
     return run()
-
 
 if __name__ == '__main__':
     main()
