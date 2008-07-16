9e7f4d05c082ff190b97f6da816d41474db4e2d0
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-16 02:07:58 +0000

    Added nose collector to setup.py

--- setup.py
+++ setup.py
@@ -69,6 +69,10 @@ setup(
         'enthought.plugins',
         ],
     packages = find_packages(exclude=['examples']),
+    tests_require = [
+        'nose >= 0.9',
+    ],
+    test_suite = 'nose.collector',
     url = 'http://code.enthought.com/envisage',
     version = '3.0.0b1',
     zip_safe = False,
