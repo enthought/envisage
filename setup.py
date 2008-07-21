baebe2069ebac0500db1ea94972a7cb0928fc694
ccasey <ccasey@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-21 22:05:46 +0000

    Moved doc files to docs/source directory and created html.zip file which contains zipped version of current html docs. Added check for html.zip file in setup.py. Fixed a very inappropriate apostrophe.              -code read, Peter Wang

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from distutils.command.build import build as distbuild
 from distutils import log
 from pkg_resources import require, DistributionNotFound
 from make_docs import HtmlBuild
+import zipfile
 
 # Function to convert simple ETS project names and versions to a requirements
 # spec that works for both development builds and stable builds.  Allows
@@ -29,17 +30,17 @@ TRAITS = etsdep('Traits', '3.0.0b1')
 def generate_docs():
     """If sphinx is installed, generate docs.
     """
-    doc_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)),'docs')
+    doc_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)),'docs',
+                           'source')
+    html_zip = os.path.join(os.path.abspath(os.path.dirname(__file__)),'docs',
+                            'html.zip')
     dest_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-        'build', 'docs')
+                            'docs')
     
-    if not os.path.exists(dest_dir):
-        os.makedirs(dest_dir)
-        
     try:
         require("Sphinx>=0.4.1")
             
-        log.info("Auto-generating documentation in EnvisageCore/build/docs...")
+        log.info("Auto-generating documentation in %s/html" % dest_dir)
         doc_src = doc_dir
         target = dest_dir
         try:
@@ -56,10 +57,42 @@ def generate_docs():
             del build
         except:
             log.error("The documentation generation failed."
-                      " RST docs located in EnvisageCore/docs.")
+                      " Looking for zip file.")
+            
+            # Unzip the docs into the 'html' folder.
+            if os.path.exists(html_zip):
+                unzip_html_docs(html_zip, dest_dir)
+                log.error("HTML documentation installed from zip.")
+            else:
+                log.error("Zip file not found. HTML documentation not yet available.")
+            
     except DistributionNotFound:
         log.error("Sphinx is not installed, so the documentation could not be "
-                  "generated.  RST docs located in EnvisageCore/docs.")
+                  "generated.  Looking for zip file...")
+        
+        # Unzip the docs into the 'html' folder.
+        if os.path.exists(html_zip):
+            unzip_html_docs(html_zip, dest_dir)
+            log.error("HTML documentation installed from zip.")
+        else:
+            log.error("Zip file not found. HTML documentation not yet available.")
+
+def unzip_html_docs(src_path, dest_dir):
+    """Given a path to a zipfile, extract
+    its contents to a given 'dest_dir'.
+    """
+    file = zipfile.ZipFile(src_path)
+    for name in file.namelist():
+        cur_name = os.path.join(dest_dir, name)
+        if not name.endswith('/'):
+            out = open(cur_name, 'wb')
+            out.write(file.read(name))
+            out.flush()
+            out.close()
+        else:
+            if not os.path.exists(cur_name):
+                os.mkdir(cur_name)
+    file.close()
 
 class my_develop(develop):
     def run(self):
