3921a9e250e32ce36213da9dfdb63d52740afd09
ccasey <ccasey@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-22 20:35:47 +0000

    Adding setup_data.py with name and version info. Adding lines using this file into setup.py. Now the version can be changed in setup_data.py and we don't have to worry about changing sphinx's conf.py file to update the documentation.

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from distutils.command.build import build as distbuild
 from distutils import log
 from pkg_resources import require, DistributionNotFound
 
+from setup_data import INFO
 from make_docs import HtmlBuild
 
 # Function to convert simple ETS project names and versions to a requirements
@@ -53,6 +54,8 @@ def generate_docs():
                 'target': target,
                 'verbose': True,
                 'versioned': False,
+                'version': INFO['version'],
+                'release': INFO['version']
                 }, [])
             del build
         except:
@@ -138,7 +141,7 @@ setup(
         ],
     test_suite = 'nose.collector',
     url = "http://code.enthought.com/envisage",
-    version = "3.0.0b1",
+    version = INFO['version'],
     zip_safe = False,
     )
 
---
+++ setup_data.py
@@ -0,0 +1,4 @@
+INFO = {
+    'name': 'EnvisageCore',
+    'version': '3.0.0b1',
+    }
