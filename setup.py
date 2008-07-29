323b03e4ca00fb0c1b1f227933f5f1d0da36d589
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-29 22:48:20 +0000

    * Added / edited project meta-data in preparation for pushing into PyPi.

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@
 """
 Extensible Application Framework
 
-Envisage is a Python-based framework for building extensible applications, 
+Envisage is a Python-based framework for building extensible applications,
 that is, applications whose functionality can be extended by adding "plug-ins".
 Envisage provides a standard mechanism for features to be added to an
 application, whether by the original developer or by someone else. In fact,
 when you build an application using Envisage, the entire application consists
-primarily of plug-ins. In this respect, it is similar to the Eclipse and 
+primarily of plug-ins. In this respect, it is similar to the Eclipse and
 Netbeans frameworks for Java applications.
 
 Each plug-in is able to:
@@ -22,7 +22,7 @@ Each plug-in is able to:
 - Create and share the objects that perform the real work of the application
   ("services").
 
-The EnvisageCore project provides the basic machinery of the Envisage 
+The EnvisageCore project provides the basic machinery of the Envisage
 framework. This project contains no plug-ins. You are free to use:
 
 - plug-ins from the EnvisagePlugins project
@@ -30,15 +30,23 @@ framework. This project contains no plug-ins. You are free to use:
 - plug-ins that you create yourself
 
 """
-import os, zipfile
+
+
+from distutils import log
+from distutils.command.build import build as distbuild
+from make_docs import HtmlBuild
+from pkg_resources import DistributionNotFound, parse_version, require, \
+    VersionConflict
+from setup_data import INFO
 from setuptools import setup, find_packages
 from setuptools.command.develop import develop
-from distutils.command.build import build as distbuild
-from distutils import log
-from pkg_resources import DistributionNotFound, parse_version, require, VersionConflict 
+import os
+import zipfile
 
-from setup_data import INFO
-from make_docs import HtmlBuild
+
+# Pull the description values for the setup keywords from our file docstring.
+DOCLINES = __doc__.split("\n")
+
 
 # Function to convert simple ETS project names and versions to a requirements
 # spec that works for both development builds and stable builds.  Allows
@@ -60,14 +68,16 @@ APPTOOLS = etsdep('AppTools', '3.0.0b1')
 ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.0b1')
 TRAITS = etsdep('Traits', '3.0.0b1')
 
+
+# Functions to generate docs from source when building this project.
 def generate_docs():
-    """If sphinx is installed, generate docs.
+    """ If sphinx is installed, generate docs.
     """
     doc_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'docs')
     source_dir = os.path.join(doc_dir, 'source')
     html_zip = os.path.join(doc_dir,  'html.zip')
     dest_dir = doc_dir
-    
+
     required_sphinx_version = "0.4.1"
     sphinx_installed = False
     try:
@@ -75,21 +85,23 @@ def generate_docs():
         sphinx_installed = True
     except (DistributionNotFound, VersionConflict):
         log.warn('Sphinx install of version %s could not be verified.'
-                    ' Trying simple import...' % required_sphinx_version)
+            ' Trying simple import...' % required_sphinx_version)
         try:
             import sphinx
-            if parse_version(sphinx.__version__) < parse_version(required_sphinx_version):
-                log.error("Sphinx version must be >=%s." % required_sphinx_version)
+            if parse_version(sphinx.__version__) < parse_version(
+                required_sphinx_version):
+                log.error("Sphinx version must be >=" + \
+                    "%s." % required_sphinx_version)
             else:
                 sphinx_installed = True
         except ImportError:
             log.error("Sphnix install not found.")
-    
-    if sphinx_installed:             
+
+    if sphinx_installed:
         log.info("Generating %s documentation..." % INFO['name'])
         docsrc = source_dir
         target = dest_dir
-        
+
         try:
             build = HtmlBuild()
             build.start({
@@ -102,11 +114,11 @@ def generate_docs():
                 'versioned': False
                 }, [])
             del build
-            
+
         except:
             log.error("The documentation generation failed.  Falling back to "
-                      "the zip file.")
-            
+                "the zip file.")
+
             # Unzip the docs into the 'html' folder.
             unzip_html_docs(html_zip, doc_dir)
     else:
@@ -115,8 +127,7 @@ def generate_docs():
         unzip_html_docs(html_zip, doc_dir)
 
 def unzip_html_docs(src_path, dest_dir):
-    """Given a path to a zipfile, extract
-    its contents to a given 'dest_dir'.
+    """ Given a path to a zipfile, extract its contents to a given 'dest_dir'.
     """
     file = zipfile.ZipFile(src_path)
     for name in file.namelist():
@@ -134,18 +145,33 @@ def unzip_html_docs(src_path, dest_dir):
 class my_develop(develop):
     def run(self):
         develop.run(self)
-        # Generate the documentation.
         generate_docs()
 
 class my_build(distbuild):
     def run(self):
         distbuild.run(self)
-        # Generate the documentation.
         generate_docs()
 
+
+# The actual setup function call.
 setup(
-    author = "Martin Chilvers",
+    author = "Martin Chilvers, et. al.",
     author_email = "info@enthought.com",
+    classifiers = [c.strip() for c in """\
+        Development Status :: 4 - Beta
+        Intended Audience :: Developers
+        Intended Audience :: Science/Research
+        License :: OSI Approved :: BSD License
+        Operating System :: MacOS
+        Operating System :: Microsoft :: Windows
+        Operating System :: OS Independent
+        Operating System :: POSIX
+        Operating System :: Unix
+        Programming Language :: Python
+        Topic :: Scientific/Engineering
+        Topic :: Software Development
+        Topic :: Software Development :: Libraries
+        """.splitlines()],
     cmdclass = {
         'develop': my_develop,
         'build': my_build
@@ -153,7 +179,7 @@ setup(
     dependency_links = [
         'http://code.enthought.com/enstaller/eggs/source',
         ],
-    description = "Envisage - An Extensible Application Framework",
+    description = DOCLINES[1],
     entry_points = """
         [enthought.envisage.plugins]
         core = enthought.envisage.core_plugin:CorePlugin
@@ -172,12 +198,16 @@ setup(
         TRAITS,
         ],
     license = "BSD",
+    long_description = '\n'.join(DOCLINES[3:]),
+    maintainer = 'ETS Developers',
+    maintainer_email = 'enthought-dev@enthought.com',
     name = "EnvisageCore",
     namespace_packages   = [
         "enthought",
         "enthought.envisage"
         ],
     packages = find_packages(),
+    platforms = ["Windows", "Linux", "Mac OS-X", "Unix", "Solaris"],
     tests_require = [
         'nose >= 0.10.3',
         ],
