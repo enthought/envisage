b59234c6d99e5587102480139d6dd113f35ed7be
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-08-07 20:30:46 +0000

    * Synchronized version numbers for ETS 3.0.0 release.

--- setup_data.py
+++ setup_data.py
@@ -14,12 +14,12 @@ def etsdep(p, min, max=None, literal=False):
 
 
 # Declare our ETS project dependencies.
-APPTOOLS = etsdep('AppTools', '3.0.0b2')
-CHACO = etsdep('Chaco', '3.0.0b2')
-ENVISAGECORE = etsdep('EnvisageCore', '3.0.0b2')
-ETSDEVTOOLS_FBI = etsdep('ETSDevTools[fbi]', '3.0.0b2')  # -- only by the debug/fbi_plugin.py
-TRAITSGUI = etsdep('TraitsGUI', '3.0.1b1')
-TRAITS_UI = etsdep('Traits[ui]', '3.0.1b1')
+APPTOOLS = etsdep('AppTools', '3.0.0')
+CHACO = etsdep('Chaco', '3.0.0')
+ENVISAGECORE = etsdep('EnvisageCore', '3.0.0')
+ETSDEVTOOLS_FBI = etsdep('ETSDevTools[fbi]', '3.0.0')  # -- only by the debug/fbi_plugin.py
+TRAITSGUI = etsdep('TraitsGUI', '3.0.1')
+TRAITS_UI = etsdep('Traits[ui]', '3.0.1')
 
 
 # A dictionary of the setup data information.
