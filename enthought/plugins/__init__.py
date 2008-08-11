f5874fe1c087585e7ad5208db28f647854f000ff
swisher <swisher@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-08-11 19:36:50 +0000

    Added the project to the package's docstring. This is obvious when looking at code, but not when looking at API docs.

--- enthought/plugins/__init__.py
+++ enthought/plugins/__init__.py
@@ -2,7 +2,9 @@
 # Copyright (c) 2007 by Enthought, Inc.
 # All rights reserved.
 #------------------------------------------------------------------------------
-
+""" Plug-ins for the Envisage application framework.
+    Part of the EnvisagePlugins project of the Enthought Tool Suite.
+"""
 try:
     __import__('pkg_resources').declare_namespace(__name__)
 except:
