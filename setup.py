fd7ea9fc8a4f834cb72f5835bd22611d457c3951
ccasey <ccasey@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-10-10 22:23:01 +0000

    Added version information to setup_requires so that it can be updated when there is a bug fix of the package depended upon, which in turn should automatically update.

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ setup(
         ],
     packages = find_packages(exclude=['examples']),
     platforms = ["Windows", "Linux", "Mac OS-X", "Unix", "Solaris"],
-    setup_requires = 'setupdocs',
+    setup_requires = 'setupdocs>=1.0',
     tests_require = [
         'nose >= 0.10.3',
         ],
