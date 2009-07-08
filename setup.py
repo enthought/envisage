4a22ac99a9bc7675b126edc749bf17f8d455b2c6
GaelVaroquaux <GaelVaroquaux@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-07-08 02:05:20 +0000

    ENH: Capture any errors in setupdocs and fail gracefully during the build.

--- setup.py
+++ setup.py
@@ -36,9 +36,8 @@ If you want to build EnvisageCore from source, you must first install
 
 """
 
-
-import os
-import zipfile
+import traceback
+import sys
 
 from distutils import log
 from distutils.command.build import build as distbuild
@@ -61,13 +60,21 @@ DOCLINES = __doc__.split("\n")
 class MyDevelop(develop):
     def run(self):
         develop.run(self)
-        self.run_command('build_docs')
+        try:
+            self.run_command('build_docs')
+        except:
+            log.warn("Couldn't build documentation:\n%s" %
+                     traceback.format_exception(*sys.exc_info()))
 
 
 class MyBuild(distbuild):
     def run(self):
         distbuild.run(self)
-        self.run_command('build_docs')
+        try:
+            self.run_command('build_docs')
+        except:
+            log.warn("Couldn't build documentation:\n%s" %
+                     traceback.format_exception(*sys.exc_info()))
 
 
 # The actual setup function call.
