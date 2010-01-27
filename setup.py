02c6514cf7106f4d2f9bf3d6962691e6567c559f
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-01-27 21:01:55 +0000

    adding missing images to package data

--- setup.py
+++ setup.py
@@ -106,7 +106,9 @@ setup(
     extras_require = INFO['extras_require'],
     ext_modules = [],
     include_package_data = True,
-    package_data = {'enthought': ['envisage/ui/workbench/*.ini']},
+    package_data = {'enthought': [
+            'envisage/ui/workbench/*.ini',
+            'envisage/ui/workbench/action/images/*.png']},
     install_requires = INFO['install_requires'],
     license = 'BSD',
     long_description = '\n'.join(DOCLINES[3:]),
