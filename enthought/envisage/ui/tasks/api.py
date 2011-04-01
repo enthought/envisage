683cdcc9d18444e5e006a575f90ba0fa9098a132
epatters <epatters@enthought.com>
2011-04-01 13:49:33 -0500

    Added missing class to Tasks API.

--- enthought/envisage/ui/tasks/api.py
+++ enthought/envisage/ui/tasks/api.py
@@ -2,4 +2,5 @@ from preferences_category import PreferencesCategory
 from preferences_pane import PreferencesPane
 from task_extension import TaskExtension
 from task_factory import TaskFactory
+from task_window import TaskWindow
 from tasks_application import TasksApplication
