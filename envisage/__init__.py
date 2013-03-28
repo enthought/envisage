5dedd20ae7438327da18c6ef920a561f5568068a
Prabhu Ramachandran <prabhu@enthought.com>
2013-03-28 21:22:04 +0530

    REL: Updating version for 4.3.0 release.

--- envisage/__init__.py
+++ envisage/__init__.py
@@ -1,7 +1,7 @@
-# Copyright (c) 2007-2012 by Enthought, Inc.
+# Copyright (c) 2007-2013 by Enthought, Inc.
 # All rights reserved.
 
-__version__ = '4.2.1'
+__version__ = '4.3.0'
 
 __requires__ = [
     'apptools',
