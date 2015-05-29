82ba9f9fbd122d748a9af62721f177298d4ce74d
Pietro Berkes <pberkes@enthought.com>
2015-05-29 11:49:38 +0100

    Docstrings for the internal IP kernel.

--- envisage/plugins/ipython_kernel/internal_ipkernel.py
+++ envisage/plugins/ipython_kernel/internal_ipkernel.py
@@ -8,39 +8,57 @@ from IPython.lib.kernel import connect_qtconsole
 from IPython.kernel.zmq.kernelapp import IPKernelApp
 
 
-def mpl_kernel(gui):
-    """ Launch and return an IPython kernel with matplotlib support for the
-    desired gui.
+def mpl_kernel(gui_backend):
+    """ Launch and return an IPython kernel with matplotlib support.
 
+    Parameters
+    ----------
+    gui_backend -- string
+      The GUI mode used to initialize the matplotlib mode. For options, see
+      the `ipython --matplotlib` help pages.
     """
+
     kernel = IPKernelApp.instance()
-    kernel.initialize(['python', '--pylab=%s' % gui])
+    kernel.initialize(['python', '--pylab=%s' % gui_backend])
     return kernel
 
 
 class InternalIPKernel(object):
+    """ Represents an IPython kernel and the consoles attached to it.
+    """
 
-    def init_ipkernel(self, backend):
+    def init_ipkernel(self, gui_backend):
+        """ Initialize the IPython kernel.
+
+        Parameters
+        ----------
+        gui_backend -- string
+          The GUI mode used to initialize the matplotlib mode. For options, see
+          the `ipython --matplotlib` help pages.
+        """
         # Start IPython kernel with GUI event loop and mpl support
-        self.ipkernel = mpl_kernel(backend)
+        self.ipkernel = mpl_kernel(gui_backend)
         # To create and track active qt consoles
         self.consoles = []
 
         # This application will also act on the shell user namespace
         self.namespace = self.ipkernel.shell.user_ns
 
-        # self.namespace['ipkernel'] = self.ipkernel  # dbg
-
     def new_qt_console(self, evt=None):
-        """start a new qtconsole connected to our kernel"""
+        """ Start a new qtconsole connected to our kernel. """
         return connect_qtconsole(
             self.ipkernel.connection_file, profile=self.ipkernel.profile
         )
 
     def cleanup_consoles(self, evt=None):
+        """ Kill all existing consoles. """
         for c in self.consoles:
             c.kill()
 
     def shutdown(self):
+        """ Shutdown the kernel.
+
+        Existing IPyhton consoles are killed first.
+        """
         self.cleanup_consoles()
         self.ipkernel.shell.exit_now = True
