a04832e6a6504e7c691433d648f740f310f1fc6d
Poruri Sai Rahul <rporuri@enthought.com>
2019-01-31 19:37:21 +0530

    DEV : bump version number back to 4.8.0.dev
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ import subprocess
 from setuptools import setup, find_packages
 
 MAJOR = 4
-MINOR = 7
-MICRO = 1
+MINOR = 8
+MICRO = 0
 
-IS_RELEASED = True
+IS_RELEASED = False
 
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
