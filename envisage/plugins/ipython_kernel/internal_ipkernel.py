7ca572155fd7cafd56bec36b144a1bb1e8e03806
Mark Dickinson <mdickinson@enthought.com>
2019-09-10 11:56:47 +0100

    More cleanup; but we're now leaking a shell on Python 3 and failing on Python 2.

--- envisage/plugins/ipython_kernel/internal_ipkernel.py
+++ envisage/plugins/ipython_kernel/internal_ipkernel.py
@@ -6,19 +6,14 @@ https://github.com/ipython/ipython/blob/2.x/examples/Embedding/internal_ipkernel
 from __future__ import absolute_import, print_function, unicode_literals
 
 from distutils.version import StrictVersion as Version
-import sys
 
 import ipykernel
 from ipykernel.connect import connect_qtconsole
 from ipykernel.ipkernel import IPythonKernel
 from ipykernel.zmqshell import ZMQInteractiveShell
-import IPython.utils.io
-from tornado import ioloop
 
 from envisage.plugins.ipython_kernel.kernelapp import IPKernelApp
 from traits.api import Any, HasStrictTraits, Instance, List
-
-NEEDS_IOLOOP_PATCH = Version(ipykernel.__version__) >= Version('4.7.0')
 
 
 # XXX Refactor to avoid the fragile subclassing of IPKernelApp. (One day.)
@@ -27,10 +22,6 @@ NEEDS_IOLOOP_PATCH = Version(ipykernel.__version__) >= Version('4.7.0')
 #     based cleanup to reclaim fds.
 # XXX Can we double check that _all_ sockets created are being closed
 #     explicitly, rather than as a result of refcounting?
-# XXX By the time that the Shell stores the sys state (InteractiveShell.save_sys_module_state),
-#     it's already been swapped out for something else. Why? Who's setting the sys
-#     attributes *before* save_sys_module_state gets called, and why?
-#     Answer: see self.init_io in the kernelapp.
 # XXX Check list of *all* objects for anything suspiciously IPython-like.
 #     What's the delta between the zeroth run and the first?
 # XXX Consider avoiding the "instance" singleton stuff. Maybe save that
@@ -38,6 +29,7 @@ NEEDS_IOLOOP_PATCH = Version(ipykernel.__version__) >= Version('4.7.0')
 # XXX Move kernelapp related stuff from here to kernelapp.
 # XXX Testing: verify that the displayhook, excepthook and other sys pieces
 #     have been restored.
+# XXX Testing: verify that IPython.utils.io unaltered (stdin, too)
 
 
 def gui_kernel(gui_backend):
@@ -80,15 +72,8 @@ class InternalIPKernel(HasStrictTraits):
     #: This is a list of tuples (name, value).
     initial_namespace = List()
 
-    #: Original sys.modules["__main__"]
-    _original_modules_main = Any()
-
     #: old value for the _ctrl_c_message, so that it can be restored
     _original_ctrl_c_message = Any()
-
-    _original_ipython_utils_io_stdin = Any()
-    _original_ipython_utils_io_stdout = Any()
-    _original_ipython_utils_io_stderr = Any()
 
     def init_ipkernel(self, gui_backend):
         """ Initialize the IPython kernel.
@@ -101,19 +86,6 @@ class InternalIPKernel(HasStrictTraits):
         """
         # More global state modification.
 
-        # XXX Add general save-and-restore machinery, that copes with the
-        # case where (a) the attribute does not exist; (b) the attribute
-        # does exist, but is None, and (c) the attribute does exist, but
-        # is not None. Cleanup may need to actually delete an attribute
-        # that didn't previously exist.
-        self._original_ipython_utils_io_stdin = IPython.utils.io.stdin
-        self._original_ipython_utils_io_stdout = IPython.utils.io.stdout
-        self._original_ipython_utils_io_stderr = IPython.utils.io.stderr
-
-        # XXX Move this code! The kernelapp itself should be responsible
-        # for resetting these cleanly.
-        self._original_modules_main = sys.modules["__main__"]
-
         # Suppress the unhelpful "Ctrl-C will not work" message from the
         # kernelapp.
         self._original_ctrl_c_message = getattr(
@@ -123,14 +95,6 @@ class InternalIPKernel(HasStrictTraits):
 
         # Start IPython kernel with GUI event loop support
         self.ipkernel = gui_kernel(gui_backend)
-
-        # Since ipykernel 4.7, the io_loop attribute of the kernel is not
-        # initialized anymore
-        # Reference: https://github.com/enthought/envisage/issues/107
-        # Workaround: Retrieve the kernel on the IPykernelApp and set the
-        # io_loop without starting it!
-        if NEEDS_IOLOOP_PATCH and not hasattr(self.ipkernel.kernel, 'io_loop'):
-            self.ipkernel.kernel.io_loop = ioloop.IOLoop.instance()
 
         # This application will also act on the shell user namespace
         self.namespace = self.ipkernel.shell.user_ns
@@ -159,14 +123,6 @@ class InternalIPKernel(HasStrictTraits):
         if self.ipkernel is not None:
             self.cleanup_consoles()
 
-            # XXX not quite right; it's the shell that's messing with this.
-            # XXX The upstream code doesn't use __main__ here; should we
-            # be doing something different?
-            kernel_sys_modules_main = sys.modules["__main__"]
-            if kernel_sys_modules_main is not self._original_modules_main:
-                sys.modules["__main__"] = self._original_modules_main
-                self._original_modules_main = None
-
             self.ipkernel.shutdown()
             self.ipkernel = None
 
@@ -176,15 +132,7 @@ class InternalIPKernel(HasStrictTraits):
                     self._original_ctrl_c_message)
                 self._original_ctrl_c_message = None
 
-            IPython.utils.io.stdout = self._original_ipython_utils_io_stdin
-            IPython.utils.io.stderr = self._original_ipython_utils_io_stderr
-            IPython.utils.io.stdin = self._original_ipython_utils_io_stdin
-
-            self._original_ipython_utils_io_stdin = None
-            self._original_ipython_utils_io_stdout = None
-            self._original_ipython_utils_io_stderr = None
-
-            # Remove singletons.
+            # Remove singletons to facilitate garbage collection.
             ZMQInteractiveShell.clear_instance()
             IPythonKernel.clear_instance()
             IPKernelApp.clear_instance()
--- envisage/plugins/ipython_kernel/kernelapp.py
+++ envisage/plugins/ipython_kernel/kernelapp.py
@@ -9,7 +9,8 @@ import atexit
 import logging
 import sys
 
-from ipykernel.kernelapp import IPKernelApp as UpstreamIPKernelApp
+import ipykernel.kernelapp
+import IPython.utils.io
 import six
 import zmq
 
@@ -28,7 +29,7 @@ else:
     from atexit import unregister as atexit_unregister
 
 
-class IPKernelApp(UpstreamIPKernelApp):
+class IPKernelApp(ipykernel.kernelapp.IPKernelApp):
     """
     Patched version of the IPKernelApp, mostly to support clean shutdown.
     """
@@ -110,30 +111,18 @@ class IPKernelApp(UpstreamIPKernelApp):
 
         super(IPKernelApp, self).init_io()
 
+    def init_shell(self):
+        """
+        Create the InteractiveShell for this kernel.
+
+        Extended to store the original values of IPython.utils.io.stdout
+        and IPython.utils.io.stderr, so that they can be restored later.
+        """
+        self._original_ipython_utils_io_stdout = IPython.utils.io.stdout
+        self._original_ipython_utils_io_stderr = IPython.utils.io.stderr
+        super(IPKernelApp, self).init_shell()
+
     # New methods, mostly to control shutdown #################################
-
-    def close_crash_handler(self):
-        sys.excepthook = self._original_sys_excepthook
-        del self._original_sys_excepthook
-
-    def close_io(self):
-        # XXX Important that this happens *before* the IOPubThread is shut
-        # down: otherwise, writes to stderr and stdout use the thread.
-        # Where is the iopub_thread set up? As part of init_sockets, and
-        # indeed we close the sockets *after* closing io.
-
-        if self.displayhook_class:
-            sys.displayhook = self._original_sys_displayhook
-            del self._original_sys_displayhook
-
-        if self.outstream_class:
-            sys.stderr.close()
-            sys.stderr = self._original_sys_stderr
-            del self._original_sys_stderr
-
-            sys.stdout.close()
-            sys.stdout = self._original_sys_stdout
-            del self._original_sys_stdout
 
     def shutdown(self):
         """
@@ -153,50 +142,95 @@ class IPKernelApp(UpstreamIPKernelApp):
 
         self.close_crash_handler()
 
-    def _unbind_socket(self, s, port):
-        transport = self.transport
+    def close_shell(self):
+        # clean up and close the shell attached to the kernel
+        shell = self.kernel.shell
 
-        if port <= 0:
-            # This should only happen if _bind_socket hasn't been called.
-            # So we shouldn't ever get here, but perhaps we should defensively
-            # do nothing (maybe print a warning about unbinding a port
-            # that was never bound) if we do get here.
-            raise RuntimeError("Shouldn't ever get here.")
+        # Clean up script magics object, which is contained in a reference
+        # cycle, and has a __del__ method, preventing its removal in Python 2.
+        magics_manager = shell.magics_manager
+        script_magics = magics_manager.registry["ScriptMagics"]
 
-        # XXX Repetition of information from _bind_socket; refactor
-        # to remove the repetition.
-        if transport == "tcp":
-            endpoint = "tcp://%s:%i" % (self.ip, port)
-        elif transport == "ipc":
-            endpoint = "ipc://%s-%i" % (self.ip, port)
-        else:
-            raise ValueError(
-                "Unknown transport type: {}".format(self.transport))
+        script_magics.kill_bg_processes()
+        atexit_unregister(script_magics.kill_bg_processes)
 
-        s.unbind(endpoint)
+        script_magics.magics.clear()
+        script_magics.shell = None
+        script_magics.parent = None
 
-    def close_sockets(self):
+        shell.history_manager.end_session()
+        # The stop also cleans up the file connection.
+        shell.history_manager.save_thread.stop()
+        # Don't run again at process shutdown.
+        atexit_unregister(shell.history_manager.save_thread.stop)
+        # Rely on garbage collection to clean up the file connection.
+        shell.history_manager.db.close()
+
+        # Undo changes to IPython.utils.io made at shell creation time.
+        # The values written by the shell keep references that prevent
+        # proper garbage collection from taking place.
+        IPython.utils.io.stderr = self._original_ipython_utils_io_stderr
+        del self._original_ipython_utils_io_stderr
+
+        IPython.utils.io.stdout = self._original_ipython_utils_io_stdout
+        del self._original_ipython_utils_io_stdout
+
+        # The shell's cleanup method restores the sys.module changes.
+        shell.cleanup()
+
+        # atexit_operations does:
+        # - history_manager end session (called above)
+        # - remove temp files and directories
+        # - clear user namespaces
+        # - run user shutdown hooks
+        # XXX Do we want to do all these things? Should we just call
+        # atexit_operations directly?
+        atexit_unregister(shell.atexit_operations)
+
+        # Remove some references to avoid keeping objects alive
+        # unnecessarily.
+        del shell.configurables[:]
+        del shell.sys_excepthook
+        del shell._orig_sys_module_state
+        del shell._orig_sys_modules_main_mod
+
+        # Clear the stored singleton to faciliate garbage collection.
+        # shell.clear_instance()
+
+    def close_kernel(self):
         """
-        Unbind, close and destroy sockets created by init_sockets.
+        Undo setup from init_kernel.
         """
-        # context = zmq.Context.instance()
-        self.close_iopub()
+        # Dig in to find the shell stream and control stream.
+        # kernel is a an IPythonKernel object
 
-        # XXX Already closed by close_kernel, but not explictly unbound.
-        # unbind after close fails (not surprisingly)
-        # Do we still need to unbind?
-        #self._unbind_socket(self.control_socket, self.control_port)
-        #self.control_socket.close()
-        del self.control_socket
+        # XXX This is ugly: ideally, we should be closing the streams
+        # in the same class that creates them. That may be awkward
+        # in practice.
+        kernel = self.kernel
+        for stream in kernel.shell_streams:
+            stream.stop_on_recv()
+            # This also closes the corresponding socket.
+            stream.close()
 
-        self._unbind_socket(self.stdin_socket, self.stdin_port)
-        self.stdin_socket.close()
-        del self.stdin_socket
+    def close_io(self):
+        # XXX Important that this happens *before* the IOPubThread is shut
+        # down: otherwise, writes to stderr and stdout use the thread.
+        # Where is the iopub_thread set up? As part of init_sockets, and
+        # indeed we close the sockets *after* closing io.
 
-        # Also already closed when closing the associated ZMQStream
-        #self._unbind_socket(self.shell_socket, self.shell_port)
-        #self.shell_socket.close()
-        del self.shell_socket
+        if self.displayhook_class:
+            sys.displayhook = self._original_sys_displayhook
+            del self._original_sys_displayhook
+
+        if self.outstream_class:
+            sys.stderr.close()
+            sys.stderr = self._original_sys_stderr
+            del self._original_sys_stderr
+
+            sys.stdout.close()
+            sys.stdout = self._original_sys_stdout
+            del self._original_sys_stdout
 
     def close_iopub(self):
         """
@@ -224,90 +258,56 @@ class IPKernelApp(UpstreamIPKernelApp):
         self.iopub_socket.close()
         del self.iopub_socket
 
-    def close_kernel(self):
-        """
-        Undo setup from init_kernel.
-        """
-        # Dig in to find the shell stream and control stream.
-        # kernel is a an IPythonKernel object
-
-        # XXX This is ugly: ideally, we should be closing the streams
-        # in the same class that creates them. That may be awkward
-        # in practice.
-        kernel = self.kernel
-        for stream in kernel.shell_streams:
-            stream.stop_on_recv()
-            # This also closes the corresponding socket.
-            stream.close()
-
-    def close_shell(self):
-        # clean up and close the shell attached to the kernel
-        shell = self.kernel.shell
-
-        # Clean up script magics object, which is contained in a reference
-        # cycle, and has a __del__ method, preventing its removal in Python 2.
-        magics_manager = shell.magics_manager
-        script_magics = magics_manager.registry["ScriptMagics"]
-
-        script_magics.kill_bg_processes()
-        atexit_unregister(script_magics.kill_bg_processes)
-
-        script_magics.magics.clear()
-        script_magics.shell = None
-        script_magics.parent = None
-
-        shell.history_manager.end_session()
-        # The stop also cleans up the file connection.
-        shell.history_manager.save_thread.stop()
-        # Don't run again at process shutdown.
-        atexit_unregister(shell.history_manager.save_thread.stop)
-        # Rely on garbage collection to clean up the file connection.
-        shell.history_manager.db.close()
-        del shell.configurables[:]
-
-        del shell.sys_excepthook
-        del shell._orig_sys_module_state
-        del shell._orig_sys_modules_main_mod
-
-        # atexit_operations does:
-        # - history_manager end session (called above)
-        # - remove temp files and directories
-        # - clear user namespaces
-        # - run user shutdown hooks
-        # Do we want to do all these things? Should we just call
-        # atexit_operations directly?
-        atexit_unregister(shell.atexit_operations)
+    def close_crash_handler(self):
+        sys.excepthook = self._original_sys_excepthook
+        del self._original_sys_excepthook
 
     def close_heartbeat(self):
         # This should interrupt the zmq.device call.
         self.heartbeat.context.term()
         self.heartbeat.join()
 
-    def init_heartbeat(self):
-        """start the heart beating"""
-        # Overridden from parent to use the local Heartbeat class.
+    def close_sockets(self):
+        """
+        Unbind, close and destroy sockets created by init_sockets.
+        """
+        # context = zmq.Context.instance()
+        self.close_iopub()
 
-        # heartbeat doesn't share context, because it mustn't be blocked
-        # by the GIL, which is accessed by libzmq when freeing zero-copy
-        # messages
-        hb_ctx = zmq.Context()
-        self.heartbeat = Heartbeat(hb_ctx, (self.transport, self.ip, self.hb_port))
-        self.hb_port = self.heartbeat.port
-        self.log.debug("Heartbeat REP Channel on port: %i" % self.hb_port)
-        self.heartbeat.start()
+        # XXX Already closed by close_kernel, but not explictly unbound.
+        # unbind after close fails (not surprisingly)
+        # Do we still need to unbind?
+        #self._unbind_socket(self.control_socket, self.control_port)
+        #self.control_socket.close()
+        del self.control_socket
 
-    def patch_io(self):
-        # Overridden to do nothing. Alternatively, we need to write
-        # and call a corresponding function at teardown that undoes
-        # the faulthandler monkeypatching.
+        self._unbind_socket(self.stdin_socket, self.stdin_port)
+        self.stdin_socket.close()
+        del self.stdin_socket
 
-        # Related: https://github.com/ipython/ipykernel/issues/91
+        # Also already closed when closing the associated ZMQStream
+        #self._unbind_socket(self.shell_socket, self.shell_port)
+        #self.shell_socket.close()
+        del self.shell_socket
 
-        # Suggest that the appropriate fix for users of this class
-        # is to make sure they enable faulthandler before the
-        # IPython kernel does its sys magic.
-        pass
+    def _unbind_socket(self, s, port):
+        transport = self.transport
 
-    def configure_tornado_logger(self):
-        # Overridden to do nothing.
-        pass
+        if port <= 0:
+            # This should only happen if _bind_socket hasn't been called.
+            # So we shouldn't ever get here, but perhaps we should defensively
+            # do nothing (maybe print a warning about unbinding a port
+            # that was never bound) if we do get here.
+            raise RuntimeError("Shouldn't ever get here.")
+
+        # XXX Repetition of information from _bind_socket; refactor
+        # to remove the repetition.
+        if transport == "tcp":
+            endpoint = "tcp://%s:%i" % (self.ip, port)
+        elif transport == "ipc":
+            endpoint = "ipc://%s-%i" % (self.ip, port)
+        else:
+            raise ValueError(
+                "Unknown transport type: {}".format(self.transport))
+
+        s.unbind(endpoint)
