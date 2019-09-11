f1366703226d124926ccbabe47c14ac05fa2c7d6
poruri sai rahul <rporuri@enthought.com>
2019-09-11 13:52:11 +0100

    REF : Refactor the version gen machinery in setup.py
    Separate the tasks of getting the version info from generating the
    version info. Further, separate the tasks of getting the version
    info from the git source tree and from the version file.
    
    Finally, dont import the install_requires from the package init file.
    
    NOTE : The version file is not a private file anymore.
    
    	modified:   .gitignore
    	modified:   envisage/__init__.py
    	modified:   setup.py

--- envisage/__init__.py
+++ envisage/__init__.py
@@ -1,31 +1,17 @@
 # Copyright (c) 2007-2015 by Enthought, Inc.
 # All rights reserved.
 
-from ._version import full_version as __version__  # noqa
-
-__requires__ = [
-    'apptools',
-    'traits',
-]
+try:
+    from envisage.version import version as __version__
+except ImportError:
+    # If we get here, we're using a source tree that hasn't been created via
+    # the setup script.
+    __version__ = "unknown"
 
 # Per logging best practices, add a NullHandler to the root 'envisage'
 # logger.
 import logging
 
+logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-class NullHandler(logging.Handler):
-    # Define our own to accommodate Python < 2.7
-    def handle(self, record):
-        pass
-
-    def emit(self, record):
-        pass
-
-    def createLock(self):
-        self.lock = None
-
-
-logger = logging.getLogger(__name__)
-logger.addHandler(NullHandler())
-
-del logging, logger, NullHandler
+del logging
