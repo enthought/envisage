306e28906d110f868e602a8aa3881e1186ca6246
poruri sai rahul <rporuri@enthought.com>
2019-09-12 17:34:21 +0100

    FIX : Add license headers to all files

--- envisage/ui/single_project/services.py
+++ envisage/ui/single_project/services.py
@@ -1,12 +1,11 @@
-#-----------------------------------------------------------------------------
+# (C) Copyright 2007-2019 Enthought, Inc., Austin, TX
+# All rights reserved.
 #
-#  Copyright (c) 2006 by Enthought, Inc.
-#  All rights reserved.
-#
-#  Author: Dave Peterson <dpeterson@enthought.com>
-#
-#-----------------------------------------------------------------------------
-
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only
+# under the conditions described in the aforementioned license.  The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+# Thanks for using Enthought open source!
 # IDs of services provided by this plugin
 IPROJECT_MODEL = 'envisage.ui.single_project.model_service.ModelService'
 IPROJECT_UI = 'envisage.ui.single_project.ui_service.UiService'
