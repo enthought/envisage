e5c6e6e9d9514012cd9d46671acd173795514c95
rahul poruri <rporuri@enthought.com>
2020-01-06 07:54:29 +0000

    REF : Redefine the ID strings in ids module
    	modified:   envisage/ids.py

--- envisage/ids.py
+++ envisage/ids.py
@@ -7,40 +7,34 @@
 # is also available online at http://www.enthought.com/licenses/BSD.txt
 # Thanks for using Enthought open source!
 
-from envisage.core_plugin import CorePlugin
-from envisage.plugins.ipython_kernel.ipython_kernel_plugin import (
-    IPythonKernelPlugin
-)
-from envisage.plugins.python_shell.python_shell_plugin import PythonShellPlugin
-from envisage.ui.tasks.tasks_plugin import TasksPlugin
+""" This module redefines the Extension Point IDs and Service IDs defined by
+Plugins provided by Envisage. Note that this module does not contain IDs
+defined by all of the Plugins available in envisage.
+The Plugins themselves remain the ground truth for the IDs. This module is
+simply a convenient location from which the user can import the IDs.
+"""
 
 #### Extension Points #########################################################
 
 # Extension Points defined by the CorePlugin.
-PREFERENCES = CorePlugin.PREFERENCES
-SERVICE_OFFERS = CorePlugin.SERVICE_OFFERS
+PREFERENCES = 'envisage.preferences'
+SERVICE_OFFERS = 'envisage.service_offers'
 
 # Extension Points defined by the PythonShellPlugin.
 # NOTE : The other PythonShellPlugin defines extension points with the same ID.
-BINDINGS = PythonShellPlugin.BINDINGS
-COMMANDS = PythonShellPlugin.COMMANDS
+BINDINGS = 'envisage.plugins.python_shell.bindings'
+COMMANDS = 'envisage.plugins.python_shell.commands'
 
 # Extension Points defined by the IPythonKernelPlugin.
-IPYTHON_NAMESPACE = IPythonKernelPlugin.IPYTHON_NAMESPACE
+IPYTHON_NAMESPACE = 'ipython_plugin.namespace'
 
 # Extension Points defined by the TasksPlugin.
-PREFERENCES_CATEGORIES = TasksPlugin.PREFERENCES_CATEGORIES
-PREFERENCES_PANES = TasksPlugin.PREFERENCES_PANES
-TASKS = TasksPlugin.TASKS
-TASK_EXTENSIONS = TasksPlugin.TASK_EXTENSIONS
+PREFERENCES_CATEGORIES = 'envisage.ui.tasks.preferences_categories'
+PREFERENCES_PANES = 'envisage.ui.tasks.preferences_panes'
+TASKS = 'envisage.ui.tasks.tasks'
+TASK_EXTENSIONS = 'envisage.ui.tasks.task_extensions'
 
 #### Services ################################################################
 
 # Services offered by the IPythonKernelPlugin
-IPYTHON_KERNEL_PROTOCOL = IPythonKernelPlugin.IPYTHON_KERNEL_PROTOCOL
-
-# Ensure that a `from envisage.ids import *` import only brings in ids.
-del CorePlugin
-del IPythonKernelPlugin
-del PythonShellPlugin
-del TasksPlugin
+IPYTHON_KERNEL_PROTOCOL = 'envisage.plugins.ipython_kernel.internal_ipkernel.InternalIPKernel'  # noqa: E501
