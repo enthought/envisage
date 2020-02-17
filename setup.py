117f685f826d8b8a51e5e350b7c13cda2110aa4e
Mark Dickinson <mdickinson@enthought.com>
2020-02-17 13:58:53 +0000

    Remove Python 2 trove classifier

--- setup.py
+++ setup.py
@@ -270,7 +270,6 @@ if __name__ == "__main__":
             Operating System :: Microsoft :: Windows
             Operating System :: POSIX :: Linux
             Programming Language :: Python
-            Programming Language :: Python :: 2
             Programming Language :: Python :: 3
             Programming Language :: Python :: 3.5
             Programming Language :: Python :: 3.6
