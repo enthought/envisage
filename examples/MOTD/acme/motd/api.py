9ccb3ce1be511dfc60654ea6313ce71b124e4510
Poruri Sai Rahul <rporuri@enthought.com>
2020-10-21 10:28:14 +0000

    Fix the MOTD example (#319)
    * FIX : MOTD example
    
    The fixes were mostly trivial - using range instead of xrange, using
    open instead of file and using explicit imports instead of implicit
    imports
    
    	modified:   examples/MOTD/acme/motd/api.py
    	modified:   examples/MOTD/acme/motd/message.py
    	modified:   examples/MOTD/acme/motd/motd.py
    	modified:   examples/MOTD/acme/motd/motd_plugin.py
    	modified:   examples/MOTD/acme/motd/software_quotes/software_quotes_plugin.py
    	modified:   examples/MOTD/run.py
    
    * FIX : Use FileHandler instead of StreamHandler
    
    for logging the MOTD application
    
    	modified:   run.py
    
    * CLN: utf-8, not utf8
    
    Just to stay consistent with what the rest of the codebase is doing.

--- examples/MOTD/acme/motd/api.py
+++ examples/MOTD/acme/motd/api.py
@@ -1,5 +1,5 @@
-from i_message import IMessage
-from i_motd import IMOTD
+from acme.motd.i_message import IMessage
+from acme.motd.i_motd import IMOTD
 
-from message import Message
-from motd import MOTD
+from acme.motd.message import Message
+from acme.motd.motd import MOTD
