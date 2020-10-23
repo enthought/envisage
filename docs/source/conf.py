c4768fb260346dc552f83cbb0d747fd2fdff4cf8
Poruri Sai Rahul <rporuri@enthought.com>
2020-10-23 08:11:16 +0000

    CLN : Order intersphinx alphabetically

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,8 +33,8 @@ import enthought_sphinx_theme
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
     "sphinx.ext.githubpages",
+    "sphinx.ext.intersphinx",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "traits.util.trait_documenter",
