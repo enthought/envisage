e0ae02a66a7bea9e8be2727e1dcdbfa243c959eb
Kit Choi <kitchoi@users.noreply.github.com>
2020-12-01 10:22:41 +0000

    Make example run from any directory (#377)
    * Make it possible to run example scripts from anywhere.
    
    * Add a docstring for the test case
    
    * Add missing __init__.py
    
    * Import abcdefg...

---
+++ envisage/examples/tests/test__demo.py
@@ -0,0 +1,24 @@
+# (C) Copyright 2007-2020 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only under
+# the conditions described in the aforementioned license. The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+# Thanks for using Enthought open source!
+import os
+import sys
+import unittest
+
+from envisage.examples._demo import demo_path
+
+
+class TestDemoUtilities(unittest.TestCase):
+    """ Test utility functions in the _demo module."""
+
+    def test_sys_path_inserted(self):
+        path = os.path.join("dirname", "file.py")
+        with demo_path(path):
+            self.assertIn("dirname", sys.path)
+        self.assertNotIn("dirname", sys.path)
