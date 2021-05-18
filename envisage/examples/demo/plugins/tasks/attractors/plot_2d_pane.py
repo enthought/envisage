168f76957d6fc4e95afc9adeebb2146efe967879
Aaron Ayres <aayres@enthought.com>
2021-05-18 16:49:44 -0500

    things were broken

--- envisage/examples/demo/plugins/tasks/attractors/plot_2d_pane.py
+++ envisage/examples/demo/plugins/tasks/attractors/plot_2d_pane.py
@@ -31,51 +31,54 @@ class Plot2dPane(TraitsTaskPane):
     active_model = Instance(IPlottable2d)
     models = List(IPlottable2d)
 
-    plot_type = Property(Str, observe="active_model.plot_type")
-    title = Property(Str, observe="active_model.name")
-    x_data = Property(observe="active_model.x_data")
-    y_data = Property(observe="active_model.y_data")
-    x_label = Property(Str, observe="active_model.x_label")
-    y_label = Property(Str, observe="active_model.y_label")
-
     plot = Instance(Plot)
 
     def _plot_default(self):
-        plot = Plot(ArrayPlotData(x=self.x_data, y=self.y_data))
-        plot.x_axis.title = self.x_label
-        plot.y_axis.title = self.y_label
+        plot = Plot(
+            ArrayPlotData(
+                x=self.active_model.x_data, y=self.active_model.y_data
+            )
+        )
+        plot.x_axis.title = self.active_model.x_label
+        plot.y_axis.title = self.active_model.y_label
 
         plot.plot(
-            ("x", "y"), type=self.plot_type, name=self.title, marker='pixel', color="blue"
+            ("x", "y"),
+            type=self.active_model.plot_type,
+            name=self.active_model.name,
+            marker='pixel',
+            color="blue"
         )
 
         return plot
 
-    @observe("x_data,y_data")
+    @observe("active_model:x_data,active_model:y_data")
     def _update_plot_data(self, event):
         if event.name == "x_data":
-            self.plot.data.set_data("x", self.x_data)
+            self.plot.data.set_data("x", event.new)
         else:
-            self.plot.data.set_data("y", self.y_data)
+            self.plot.data.set_data("y", event.new)
+        self.plot.invalidate_and_redraw()
 
-    @observe("x_label,y_label")
-    def _update_plot_data(self, event):
+    @observe("active_model:[x_label,y_label]")
+    def _update_axis_label(self, event):
         if event.name == "x_label":
             self.plot.x_axis.title = event.new
         else:
             self.plot.y_axis.title = event.new
+        self.plot.invalidate_and_redraw()
 
     @observe("active_model")
     def _update_plot_new_model(self, event):
         if event.old:
             self.plot.delplot(event.old.name)
-        print(event.new.plot_type)
+
+        self.plot.data.set_data("x", event.new.x_data)
+        self.plot.data.set_data("y", event.new.y_data)
         self.plot.plot(
             ("x", "y"), type=event.new.plot_type, name=event.new.name, marker='pixel', color="blue"
         )
-        print('heyyyy')
         self.plot.invalidate_and_redraw()
-        print('yoooo')
 
     view = View(
         HGroup(
@@ -92,30 +95,6 @@ class Plot2dPane(TraitsTaskPane):
     #### Private traits #######################################################
 
     _enum_map = Dict(IPlottable2d, Str)
-
-    ###########################################################################
-    # Protected interface.
-    ###########################################################################
-
-    #### Trait property getters/setters #######################################
-
-    def _get_plot_type(self):
-        return self.active_model.plot_type if self.active_model else "line"
-
-    def _get_title(self):
-        return self.active_model.name if self.active_model else ""
-
-    def _get_x_data(self):
-        return self.active_model.x_data if self.active_model else []
-
-    def _get_y_data(self):
-        return self.active_model.y_data if self.active_model else []
-
-    def _get_x_label(self):
-        return self.active_model.x_label if self.active_model else ""
-
-    def _get_y_label(self):
-        return self.active_model.y_label if self.active_model else ""
 
     #### Trait change handlers ################################################
 
