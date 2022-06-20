8a8da99371b53c1298bacd37ce7061e95aa342a5
Mark Dickinson <mdickinson@enthought.com>
2022-06-20 12:47:31 +0100

    Removing GUI

--- envisage/hanger.py
+++ envisage/hanger.py
@@ -1,18 +1,17 @@
-from pyface.api import GUI, Window
+from pyface.api import Window
+from pyface.qt import QtGui
 from traits.api import HasTraits, Instance
 
 
 class Application(HasTraits):
 
-    gui = Instance(GUI, ())
-
     window = Instance(Window)
 
     def run(self):
-        gui = self.gui
         self.window = Window()
         self.window.open()
-        gui.start_event_loop()
+        app = QtGui.QApplication.instance()
+        app.exec_()
 
     def exit(self):
         self.window.destroy()
