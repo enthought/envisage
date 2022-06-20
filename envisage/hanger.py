ed98f684289275e18a82fe51b244db13357bd76f
Mark Dickinson <mdickinson@enthought.com>
2022-06-20 11:48:15 +0100

    Version reported upstream: enthought/pyface#1135

--- envisage/hanger.py
+++ envisage/hanger.py
@@ -1,5 +1,4 @@
-from pyface.api import ApplicationWindow
-from pyface.gui import GUI
+from pyface.api import GUI, ApplicationWindow
 from traits.api import Event, HasTraits, Instance
 
 
@@ -12,35 +11,23 @@ class Application(HasTraits):
     application_initialized = Event
 
     def run(self):
-        """Run the application."""
         gui = self.gui
 
         self.window = ApplicationWindow()
         self.window.open()
 
         gui.set_trait_later(self, "application_initialized", self)
-        print("Starting event loop")
         gui.start_event_loop()
-        print("Event loop finished")
 
     def exit(self):
-        """Exits the application, closing all open windows."""
-
-        print("Exiting")
-
         self.window.destroy()
         self.window.closed = True
-
-        print("Successfully exited")
-        return True
 
 
 def main():
     app = Application()
     app.on_trait_change(lambda: app.exit(), "application_initialized")
-    print("Running application")
     app.run()
-    print("Finished running application")
 
 
 if __name__ == "__main__":
