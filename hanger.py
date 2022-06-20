9871336461c28ccbc3a23b00c58698e22e0ccf09
Mark Dickinson <mdickinson@enthought.com>
2022-06-20 09:14:49 +0100

    More reductions

--- hanger.py
+++
@@ -1,9 +0,0 @@
-from envisage.ui.tasks.api import TasksApplication
-
-def main():
-    app = TasksApplication()
-    app.on_trait_change(lambda: app.exit(force=True), "application_initialized")
-    app.run()
-
-if __name__ == "__main__":
-    main()
