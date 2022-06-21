a1a957a67e810a36028d44c7008132374ba04b38
Mark Dickinson <mdickinson@enthought.com>
2022-06-21 11:08:52 +0100

    Pyface-free version

--- envisage/hanger.py
+++ envisage/hanger.py
@@ -1,35 +1,28 @@
-import weakref
-
-from pyface.qt import QtCore, QtGui
-from pyface.ui.qt4.gui import GUI
+try:
+    from PySide6 import QtCore
+    from PySide6.QtWidgets import QApplication, QMainWindow
+except ImportError:
+    from PySide2 import QtCore
+    from PySide2.QtWidgets import QApplication, QMainWindow
 
 
 class WindowEventFilter(QtCore.QObject):
-    """An internal class that watches for certain events on behalf of the
-    Window instance.
-    """
+
+    close_later = QtCore.Signal()
 
     def __init__(self, window):
-        """Initialise the event filter."""
         QtCore.QObject.__init__(self)
-        # use a weakref to fix finalization issues with circular references
-        # we don't want to be the last thing holding a reference to the window
-        self._window = weakref.ref(window)
+        self._window = window
 
     def eventFilter(self, obj, e):
-        """Adds any event listeners required by the window."""
+        window = self._window
+        if e.type() != QtCore.QEvent.Type.Close:
+            return False
 
-        window = self._window()
-        if e.type() == QtCore.QEvent.Type.Close:
-            # Do not destroy the window during its event handler.
-            GUI.invoke_later(window.close)
-
-            if window._control is not None:
-                e.ignore()
-
-            return True
-
-        return False
+        self.close_later.emit()
+        if window._control is not None:
+            e.ignore()
+        return True
 
 
 class Window:
@@ -41,41 +34,34 @@ class Window:
         event_filter = WindowEventFilter(self)
         self._event_filter = event_filter
         self._control.installEventFilter(event_filter)
+        self._event_filter.close_later.connect(
+            self.close, QtCore.Qt.QueuedConnection)
 
     def _remove_event_listeners(self):
         self._control.removeEventFilter(self._event_filter)
         self._event_filter = None
 
     def open(self):
-        control = QtGui.QMainWindow()
+        control = QMainWindow()
         control.setEnabled(True)
         control.setVisible(True)
         self._control = control
         self._add_event_listeners()
 
     def close(self):
+        self._remove_event_listeners()
         control = self._control
         control.hide()
         control.deleteLater()
-        self._remove_event_listeners()
         self._control = None
         control.close()
 
 
-class Application:
-    def __init__(self):
-        self.window = None
-
-    def run(self):
-        app = QtGui.QApplication()
-        self.window = Window()
-        self.window.open()
-        app.exec_()
-
-
 def main():
-    app = Application()
-    app.run()
+    app = QApplication()
+    window = Window()
+    window.open()
+    app.exec_()
 
 
 if __name__ == "__main__":
