35a7dec732d0f3d56442b87e1aa47cd7ce14f027
Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
2022-11-05 14:54:09 +0530

    Fix mistake in menu group specification. (#485)
    Fixes https://github.com/enthought/mayavi/issues/1182
    
    Co-authored-by: Mark Dickinson <mdickinson@enthought.com>
    (cherry picked from commit f23ea3864a5f6ffca665d47dec755992e062029b)

---
+++ envisage/tests/test_workbench.py
@@ -0,0 +1,16 @@
+# (C) Copyright 2007-2022 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only under
+# the conditions described in the aforementioned license. The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+# Thanks for using Enthought open source!
+
+import unittest
+
+
+class TestWorkbenchDefaultAction(unittest.TestCase):
+    def test_workbench_default_action(self):
+        import envisage.ui.workbench.default_action_set  # noqa: F401
