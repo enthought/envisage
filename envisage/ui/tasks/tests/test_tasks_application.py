8bcb9f443467b35a381868495ec583b4482d1d27
Mark Dickinson <mdickinson@enthought.com>
2023-03-07 14:32:36 +0000

    Remove test skips for tests which were triggering an end-of-test-run segfault

--- envisage/ui/tasks/tests/test_tasks_application.py
+++ envisage/ui/tasks/tests/test_tasks_application.py
@@ -57,7 +57,6 @@ class TestTasksApplication(unittest.TestCase):
         self.tmpdir = tempfile.mkdtemp()
         self.addCleanup(shutil.rmtree, self.tmpdir)
 
-    @skip_with_flaky_pyside
     def test_layout_save_with_protocol_3(self):
         # Test that the protocol can be overridden on a per-application basis.
         state_location = self.tmpdir
@@ -78,7 +77,6 @@ class TestTasksApplication(unittest.TestCase):
             protocol_bytes = f.read(2)
         self.assertEqual(protocol_bytes, b"\x80\x03")
 
-    @skip_with_flaky_pyside
     def test_layout_save_creates_directory(self):
         # Test that state can still be saved if the target directory
         # doesn't exist.
@@ -100,7 +98,6 @@ class TestTasksApplication(unittest.TestCase):
         self.assertTrue(state_location.exists())
         self.assertTrue(state_path.exists())
 
-    @skip_with_flaky_pyside
     def test_layout_load(self):
         # Check we can load a previously-created state. That previous state
         # has an main window size of (492, 743) (to allow us to check that
@@ -122,7 +119,6 @@ class TestTasksApplication(unittest.TestCase):
         state = app._state
         self.assertEqual(state.previous_window_layouts[0].size, (492, 743))
 
-    @skip_with_flaky_pyside
     def test_layout_load_pickle_protocol_3(self):
         # Same as the above test, but using a state stored with pickle
         # protocol 3.
