95f40c4de03dc3de6a0e2d0e1ab4ad7c445bdc3b
Mark Dickinson <mdickinson@enthought.com>
2023-03-31 16:49:35 +0100

    Update workflows

--- graft_docs.py
+++ graft_docs.py
@@ -1,9 +1,21 @@
 """
 Helper script for gh-pages documentation updates.
 
-This script assumes that you already have the gh-pages branch checked out
-locally and that the documentation has been built and is available in
-another local directory (e.g., docs/build/html).
+Example usage
+-------------
+Assuming that the gh-pages branch has been checked out into ../docs
+(for example using `git worktree add ../docs gh-pages`), and that
+documentation has been built locally via Sphinx and is in docs/build/html:
+
+After an update to the main branch, to update the docs in the root directory
+of the gh-pages branch, do:
+
+    python graft_docs.py docs/build/html ../docs
+
+After releasing version 7.3.2 (for example) of the package, to update the
+docs in the 7.3/ subdirectory of the gh-pages branch, do:
+
+    python graft_docs.py docs/build/html ../docs --tag 7.3.2
 """
 
 import argparse
@@ -11,8 +23,21 @@ import pathlib
 import re
 import shutil
 
-#: Matcher for names of directories containing historical docs.
-OLD_DOCS_MATCHER = re.compile(r"\d+\.\d+").fullmatch
+#: Matcher for names of directories containing release docs.
+RELEASE_DOCS_DIR_MATCHER = re.compile(r"\d+\.\d+").fullmatch
+
+
+def subdir_from_tagname(version: str) -> str:
+    """
+    Map a version tag (e.g., '7.2.1') to the gh-pages subdirectory containing
+    docs for that tag (e.g., '7.2').
+    """
+    subdir = ".".join(version.split(".")[:2])
+    if not RELEASE_DOCS_DIR_MATCHER(subdir):
+        raise RuntimeError(
+            f"tagname {version} does not have the expected form"
+        )
+    return subdir
 
 
 def remove_existing_docs(docs_dir: pathlib.Path) -> None:
@@ -31,7 +56,7 @@ def remove_existing_docs(docs_dir: pathlib.Path) -> None:
             print(f"  Removing file {child}")
             child.unlink()
         elif child.is_dir():
-            if OLD_DOCS_MATCHER(child.name):
+            if RELEASE_DOCS_DIR_MATCHER(child.name):
                 print(f"  Not removing old docs directory {child}")
             else:
                 print(f"  Removing directory {child}")
@@ -64,7 +89,7 @@ def copy_new_docs(source_docs: pathlib.Path, target_dir: pathlib.Path) -> None:
             raise RuntimeError("Not a file or directory: {child}: aborting")
 
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "source",
@@ -76,9 +101,22 @@ def main():
         help="Directory containing the gh-pages checkout",
         type=pathlib.Path,
     )
+    parser.add_argument(
+        "--tag",
+        help="Release tag name (when updating for a release)",
+    )
     args = parser.parse_args()
-    remove_existing_docs(args.target)
-    copy_new_docs(args.source, args.target)
+
+    if args.tag is None:
+        target = args.target
+    else:
+        target = args.target / subdir_from_tagname(args.tag)
+        if not target.exists():
+            print(f"Creating target directory {target}")
+            target.mkdir()
+
+    remove_existing_docs(target)
+    copy_new_docs(args.source, target)
 
 
 if __name__ == "__main__":
